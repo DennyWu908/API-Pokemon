3186db630f6c4c47cbeea46378fb0b214962d6c2
Denny Wu <denny.wu87@gmail.com>
2016-12-01 20:28:59 -0800

    Add code to change position of wheel

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $("#random").removeClass('hidden');
   wheel = new wheelnav('wheelDiv');
   wheel.initWheel(["random", "color", "name", "id"]);
-  wheel.wheelRadius = 120;
+  wheel.wheelRadius = 150;
+  wheel.centerX = 300;
+  wheel.centerY = 200;
   wheel.createWheel();
 })
