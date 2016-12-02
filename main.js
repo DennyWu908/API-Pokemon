1902e15e3101beb374dbfef785ce9918d195c0f3
Denny Wu <denny.wu87@gmail.com>
2016-12-01 20:20:57 -0800

    Complete project up to second list of getting started steps

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   $("#random").removeClass('hidden');
+  wheel = new wheelnav('wheelDiv');
+  wheel.initWheel(["random", "color", "name", "id"]);
+  wheel.wheelRadius = 120;
+  wheel.createWheel();
 })
